48f06dd3ce4997351d093f351f0f1af8c25c8a89
Yunhao Wang <wangyunhao88@gmail.com>
2014-11-06 13:58:58 -0600

    Delete un-useful function

--- CoworkingSpaceTinder/public/js/index-0.js
+++ CoworkingSpaceTinder/public/js/index-0.js
@@ -1,20 +1,9 @@
     
 Parse.initialize("GI4coBdWVBWk8CexD33M9SD0GjHRTTq5jo1GnUbb", "r4yKyt6O5KiJhtMVfXraqoNZqee5JjiIBFH1fyIk");
 var currentUser = Parse.User.current();
-var geocoder = new google.maps.Geocoder();
-var objectId;
-var useCurrent = true;
 var temp = $("#inforCard").html();
 var us_temp = _.template(temp);
-var temp1 = $("#CheckCard").html();
-var us_temp1 = _.template(temp1);
-var temp2 = $("#checkLoc").html();
-var us_temp2 = _.template(temp2);
-var ShowCard = Parse.Object.extend("UserInfor");
 var usName;
-var pyrmont;
-var Add;
-var Build;
 var test;
 
 if (currentUser) {
@@ -27,8 +16,8 @@ if (currentUser) {
   $('#headerbut').html("<a data-role=\"button\" onclick=\"editProfile()\" id=\"NewData\" class=\"ui-btn ui-btn-left ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-action\">Profile</a>" 
     + "<img src=\"images/coworking.png\" height=\"25\" width=\"21\" style=\"margin-top:6px\"></img>CoWorking!" 
     + "<a id=\"LogoutBut\" data-role=\"button\" onclick=\"logOut()\" class=\"ui-btn ui-btn-right ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-back\">LogOut</a>");
-  document.getElementById("search-panel").style.display = "block";
-  $('#footerbut').html("<a data-role=\"button\" onclick=\"codeAddress()\" data-position-to=\"window\" class=\"ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-search ui-btn-icon-left ui-btn-a\" data-transition=\"pop\">Search</a>");
+  //document.getElementById("search-panel").style.display = "block";
+  //$('#footerbut').html("<a data-role=\"button\" onclick=\"codeAddress()\" data-position-to=\"window\" class=\"ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-search ui-btn-icon-left ui-btn-a\" data-transition=\"pop\">Search</a>");
   //setTimeout(function(){
     //logOut();
   //}, 300000);
@@ -98,272 +87,21 @@ function editProfile() {
 }
 
 function toggleMap() {
-  document.getElementById("map_canvas").style.display="block";
   document.getElementById("welcome").style.display = "none";
+  document.getElementById("footer").style.display="none";
+  document.getElementById("map_canvas").style.display="block";
+ 
 }
 
 //Check usecurrent function
-function initialize() {
-  if (navigator.geolocation) {
-    //alert('geolocator enabled');
-    navigator.geolocation.getCurrentPosition(initializeMap);
-  } 
-  else {
-    alert('Error: No location');
-  }
-        
-}
 
 //google place return function
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    if (results[0]) {
-      Build = results[0].name;
-      var query = new Parse.Query(ShowCard);
-      query.get(objectId, {
-        success: function(s) {
-          s.set("BuildingName", Build);
-          s.save();// The object was retrieved successfully.
-        },
-        error: function(object, error) {
-          alert('Failed to create new object, with error code: ' + error.message);
-          // The object was not retrieved successfully.
-          // error is a Parse.Error with an error code and message.
-        }
-      });
-    }
-  }
-}
+
 
 //show person  nearby the location
-function initializeMap(position) {
-  var curLat;
-  var curLong;
-  document.getElementById("inforWindow").style.display = "none";
-  document.getElementById("search-panel").style.display = "none";
-  document.getElementById("footer").style.display = "none";
-  document.getElementById("checkInWindow").style.display = "block";
-  $('#headerbut').html("<a data-role=\"button\" onclick=\"window.location.reload();\" id=\"NewData\" class=\"ui-btn ui-btn-left ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-carat-l\">Profile</a>" 
-  + "<img src=\"images/coworking.png\" height=\"25\" width=\"21\" style=\"margin-top:6px\"></img>CoWorking!" 
-  + "<a id=\"LogoutBut\" data-role=\"button\" onclick=\"logOut()\" class=\"ui-btn ui-btn-right ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-back\">LogOut</a>");
 
-  if (useCurrent == true) {
-    curLat = position.coords.latitude;
-    curLong = position.coords.longitude;
-
-    //research place neme by location
-    var latlng = new google.maps.LatLng(curLat, curLong);
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          Add = results[0].formatted_address;
-          var query = new Parse.Query(ShowCard);
-          query.get(objectId, {
-            success: function(s) {
-              s.set("Address", Add);
-              s.save();   
-            },
-            error: function(object, error) {
-              //alert('Failed to create new object, with error code: ' + error.message);
-            }
-          });
-        } 
-        else {
-          alert('No results found');
-        }
-      } 
-      else {
-        alert('Geocoder failed due to: ' + status);
-      }
-    }); 
-  } 
-  else {
-    curLat = position.lat();
-    curLong = position.lng();
-  }
-
-  pyrmont = new google.maps.LatLng(curLat, curLong);
-  var request = {
-    location: pyrmont,
-    radius: '60',
-    type: ['store']
-  };
-
-  var map = new google.maps.Map(document.getElementById('map_canvas'), {
-    center: pyrmont,
-    zoom: 15
-  });
-
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-
-  var point = new Parse.GeoPoint({
-    latitude : curLat,
-    longitude : curLong
-  });
-
-  var query = new Parse.Query(ShowCard);
-  query.get(objectId, {
-    success: function(s) {
-      s.set("Location", point);
-      s.save();// The object was retrieved successfully.
-    },
-    error: function(object, error) {
-      alert('Failed to create new object, with error code: ' + error.message);
-      // The object was not retrieved successfully.
-      // error is a Parse.Error with an error code and message.
-    }
-  });
-
-  var query = new Parse.Query(ShowCard);
-  query.withinMiles("Location", point, 0.4);
-  query.limit(10);
-  query.find({
-    success:function(results) {
-      if(results.length == 1) {
-        alert("No person around")
-      }
-      else {
-        results.forEach(function(result){
-          if (result.attributes.UserName == usName) {
-            return false;
-          }
-          var locInfo = result.attributes;
-          locInfo['ObjectId']= result.id;
-          $("#checkInWindow").append(us_temp1(locInfo));
-        });
-      }   
-    },
-    error: function(error) {
-    }
-  });
-}
-
-function codeAddress() {
-  useCurrent = false;
-  input = document.getElementById("target");
-  Add = input.value;
-  var query = new Parse.Query(ShowCard);
-  query.get(objectId, {
-    success: function(s) {
-      s.set("Address", Add);
-      s.save();// The object was retrieved successfully.
-    },
-    error: function(object, error) {
-      alert('Failed to create new object, with error code: ' + error.message);
-      // The object was not retrieved successfully.
-      // error is a Parse.Error with an error code and message.
-    }
-  });
-  geocoder.geocode({
-    'address' : Add
-  },
-  function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var temp = results[0].geometry.location;
-      initializeMap(temp);        
-    } 
-    else {
-      alert("Geocode was not successful for the following reason: " + status);
-      useCurrent = true;
-    }
-  });
-}
 
 /*function calcDistance(p1, p2){
   return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000 * 0.621371).toFixed(2);
 }*/
 
-function CheckIn() {
-  document.getElementById("inforWindow").style.display = "none";
-  document.getElementById("search-panel").style.display = "none";
-  document.getElementById("footer").style.display = "none";
-  document.getElementById("checkInWindow").style.display = "none";
-  document.getElementById("checkLocation").style.display = "block"; 
-
-  $('#headerbut').html("<a data-role=\"button\" onclick=\"window.location.reload();\" id=\"NewData\" class=\"ui-btn ui-btn-left ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-carat-l\">Profile</a>" 
-    + "<img src=\"images/coworking.png\" height=\"25\" width=\"21\" style=\"margin-top:6px\"></img>CoWorking!" 
-    + "<a id=\"LogoutBut\" data-role=\"button\" onclick=\"logOut()\" class=\"ui-btn ui-btn-right ui-corner-all ui-shadowui-btn-a ui-btn-icon-notext ui-icon-back\">LogOut</a>");
-
-  var locationName = Parse.Object.extend("Location");
-  var query = new Parse.Query(locationName);
-  query.find({
-    success:function(results) {
-      results.forEach(function(result){
-        var locName = result.attributes;
-        locName['ObjectId']= result.id;
-        $("#LocWin").append(us_temp2(locName));
-      });
-    },
-    error: function(error) {
-      // error is an instance of Parse.Error.
-    }
-  });
-}
-
-
-function getUser() {
-  document.getElementById("inforWindow").style.display = "none";
-  document.getElementById("search-panel").style.display = "none";
-  document.getElementById("footer").style.display = "none";
-  document.getElementById("checkInWindow").style.display = "block";
-  document.getElementById("checkLocation").style.display = "none";
-  $('#headerbut').html("<a data-role=\"button\" onclick=\"window.location.reload();\" id=\"NewData\" class=\"ui-btn ui-btn-left ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-carat-l\">Profile</a>" 
-    + "<img src=\"images/coworking.png\" height=\"25\" width=\"21\" style=\"margin-top:6px\"></img>CoWorking!" 
-    + "<a id=\"LogoutBut\" data-role=\"button\" onclick=\"logOut()\" class=\"ui-btn ui-btn-right ui-corner-all ui-shadow ui-btn-a ui-btn-icon-notext ui-icon-back\">LogOut</a>");
-
-  /*var query = new Parse.Query(ShowCard);
-  query.get(objectId, {
-    success: function(s) {
-      s.set("CheckIn", test);
-      s.save();
-    }
-  });*/
-  currentUser.set("location", test);
-  currentUser.save(null, {
-    success: function(user) {
-      var query = new Parse.Query(Parse.User);
-      query.equalTo("location", user.attributes.location);
-      query.find({
-        success:function(results) {
-          results.forEach(function(result){
-            if (result.attributes.username == usName) {
-              return false;
-            }
-              var locInfo = result.attributes;
-              locInfo['objectId']= result.id;
-              $("#checkInWindow").append(us_temp1(locInfo));
-              });   
-        },
-        error: function(error) {
-      // error is an instance of Parse.Error.
-        }
-      }); 
-    }
-  });
-}
-
-  /*var query = new Parse.Query(ShowCard);
-  query.equalTo("CheckIn", test);
-  query.find({
-    success:function(results) {
-      if(results.length == 0 ) {
-        alert("No person around")
-      }
-      else {
-        results.forEach(function(result){
-          if (result.attributes.UserName == usName) {
-            return false;
-          }
-          var locInfo = result.attributes;
-          locInfo['ObjectId']= result.id;
-          $("#checkInWindow").append(us_temp1(locInfo));
-        });
-      }   
-    },
-    error: function(error) {
-      // error is an instance of Parse.Error.
-    }
-  });
-}*/
