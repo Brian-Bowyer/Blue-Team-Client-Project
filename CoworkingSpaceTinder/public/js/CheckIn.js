04ff8c604e8597be7bb703b2631a29773e263bce
jeanniet13 <jeanniet13@gmail.com>
2014-11-07 17:40:49 -0600

    pushed week 1 to production

--- CoworkingSpaceTinder/public/js/CheckIn.js
+++ CoworkingSpaceTinder/public/js/CheckIn.js
@@ -1,4 +1,6 @@
-Parse.initialize("GI4coBdWVBWk8CexD33M9SD0GjHRTTq5jo1GnUbb", "r4yKyt6O5KiJhtMVfXraqoNZqee5JjiIBFH1fyIk");
+//Dev
+//Parse.initialize("GI4coBdWVBWk8CexD33M9SD0GjHRTTq5jo1GnUbb", "r4yKyt6O5KiJhtMVfXraqoNZqee5JjiIBFH1fyIk");
+Parse.initialize("lu3wCFY4M2wIjZAZL99LMvaFelwvkMC6sx4a6I6I", "x7jQv4tn9vA52wZErXuvyllOYhEnG8d6IoxkeSM0");
 var currentUser = Parse.User.current();
 var temp1 = $("#CheckCard").html();
 var us_temp1 = _.template(temp1);
