79775effbe164e225d9291c877aa4c4a351412f3
Black-Humor <bobsnake64@gmail.com>
2014-11-09 11:30:39 -0600

    Moved logged in homepage to template for easier maintenance.

--- CoworkingSpaceTinder/public/js/index.js
+++ CoworkingSpaceTinder/public/js/index.js
@@ -5,6 +5,10 @@ Parse.initialize("lu3wCFY4M2wIjZAZL99LMvaFelwvkMC6sx4a6I6I", "x7jQv4tn9vA52wZErX
 var currentUser = Parse.User.current();
 var temp = $("#inforCard").html();
 var us_temp = _.template(temp);
+var ah = $('#altHeader').html();
+var ah_temp = _.template(ah);
+var af = $("#altFooter").html();
+var af_temp = _.template(af);
 var usName;
 var test;
 
@@ -14,11 +18,14 @@ if (currentUser) {
   var cardinfo = currentUser.attributes;
   document.getElementById("map_canvas").insertAdjacentHTML("afterBegin", us_temp(cardinfo));
   //document.getElementById("map_canvas").insertAdjacentHTML("afterBegin", us_temp(cardinfo))
-  $('#headerbut').html("<a data-role=\"button\" onclick=\"editProfile()\" id=\"NewData\" class=\"ui-btn ui-mini ui-btn-left ui-corner-all ui-shadow ui-btn-a \" style=\"margin-top:20px\">Edit Profile</a>" 
-    + "<img src=\"images/coworking.png\" height=\"25\" width=\"21\" style=\"margin-top:25px\"></img>Collisions!" 
-    + "<a id=\"LogoutBut\" data-role=\"button\" onclick=\"logOut()\" class=\"ui-btn ui-mini ui-btn-right ui-corner-all ui-shadow ui-btn-a \" style=\"margin-top:20px\">Log Out</a>");
-    $('#footerbut').html("<a data-role=\"button\" onclick=\"codeAddress()\" data-position-to=\"window\" class=\"ui-btn ui-mini ui-corner-all ui-shadow ui-btn-inline ui-icon-search ui-btn-icon-left ui-btn-a\" data-transition=\"pop\">Search</a>");
+  //$('#headerbut').html("<a data-role='button' onclick='editProfile()' id='NewData' class='ui-btn ui-mini ui-btn-left ui-corner-all ui-shadow ui-btn-a ' style='margin-top:20px'>Edit Profile</a>" 
+  //  + "<img src='images/coworking.png' height='25' width='21' style='margin-top:25px'></img>Collisions!" 
+  //  + "<a id='LogoutBut' data-role='button' onclick='logOut()' class='ui-btn ui-mini ui-btn-right ui-corner-all ui-shadow ui-btn-a ' style='margin-top:20px'>Log Out</a>");
+  //  $('#footerbut').html("<a data-role='button' onclick='codeAddress()' data-position-to='window' class='ui-btn ui-mini ui-corner-all ui-shadow ui-btn-inline ui-icon-search ui-btn-icon-left ui-btn-a' data-transition='pop'>Search</a>");
   
+  $('#headerbut').html(ah_temp);
+  $('#footerbut').html(af_temp);
+
   //setTimeout(function(){
     //logOut();
   //}, 300000);
@@ -27,7 +34,7 @@ if (currentUser) {
         });*/
 }
 else{
-  $('#footerbut').html("<a href=\"#popupLogin\" data-rel=\"popup\" data-position-to=\"window\" class=\"ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-a\" data-transition=\"pop\">Log In</a>");
+  $('#footerbut').html("<a href='#popupLogin' data-rel='popup' data-position-to='window' class='ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-a' data-transition='pop'>Log In</a>");
 }
 
 // log in function
@@ -45,7 +52,7 @@ function logIn() {
 
 //logout function
 function logOut() {
-  currentUser.unset("location");
+  //currentUser.unset("location");
   currentUser.save(null, {
     success: function(user) {
       Parse.User.logOut();
